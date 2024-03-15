1c8b0dc997332fcce4bb72979b95ca40d8edbc5a
Spandana <spandana1286@gmail.com>
2024-03-15 16:30:36 -0400

    final code

--- producer.js
+++ producer.js
@@ -1,22 +1,36 @@
 const { kafka } = require("./client");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 async function init() {
   const producer = kafka.producer();
 
   console.log("Connecting Producer");
   await producer.connect();
-  console.log("Producer connected successfully");
-  await producer.send({
-    topic: "rider-updates",
-    messages:[
-       {
-        partition:0,
-        key:"location-update",
-        value:JSON.stringify({name:'tony stark', loc:'Hyderabad'});
-       }
-    ]
+  console.log("Producer Connected Successfully");
+
+  rl.setPrompt("> ");
+  rl.prompt();
+
+  rl.on("line", async (line) => {
+    const [riderName, location] = line.split(" ");
+    await producer.send({
+      topic: "rider-updates",
+      messages: [
+        {
+          partition: location.toLowerCase() === "north" ? 0 : 1,
+          key: "location-update",
+          value: JSON.stringify({ name: riderName, location }),
+        },
+      ],
+    });
+  }).on("close", async () => {
+    await producer.disconnect();
   });
-  await producer.disconnect();
 }
 
 init();
